5a23734df5becc4992d1e4f97e7632c6caca3cd7
Egor kolesnikov <egor.kolesnikov@da-14.com>
2019-04-19 14:14:13 +0300

    feat: add auth-api package

--- libs/auth-api/src/public_api.ts
+++ libs/auth-api/src/public_api.ts
@@ -1,10 +1,2 @@
 export * from './lib/auth-api.service';
 export * from './lib/auth-api.module';
-
-export * from './lib/services/app.service';
-export * from './lib/services/bot.service';
-export * from './lib/services/dashboard.service';
-export * from './lib/services/dealer.service';
-export * from './lib/services/payment.service';
-export * from './lib/services/auth-role.service';
-export * from './lib/services/auth-user.service';
